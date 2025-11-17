c9df000b38a5cdee79f7deb373637b4a29ca3fce
悲风晓月 <104625287+BeiFengXiaoyue@users.noreply.github.com>
2025-11-17 21:06:33 +0800

    Add files via upload

---
+++ 205CharStatistics.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string line;
+	int englishchars = 0, spaces = 0, numbers = 0, others = 0;
+	cout << "please input string:" << endl;
+	getline(cin, line);  // 读取整行，包括空格
+		for (char c : line) {
+			if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+				englishchars++;
+			}
+			else if (c >= '0' && c <= '9') {
+				numbers++;
+			}
+			else if (c == ' ') {
+				spaces++;
+			}
+			else {
+				others++;
+			}
+		}
+	cout << "statistic:" << endl;
+	cout << "Englishchars:" << englishchars << endl;
+	cout << "Spaces:" << spaces << endl;
+	cout << "Number:" << numbers << endl;
+	cout << "others:" << others << endl;
+	return 0;
+}
